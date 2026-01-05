e85def64676dc818721f717bf3f0c2c3fab8038f
Karri Venkata Sai Akash <84982808+akash4426@users.noreply.github.com>
2026-01-05 15:46:20 +0530

    Changes made

--- streamlit_app.py
+++ streamlit_app.py
@@ -255,13 +255,14 @@ if search:
                             </a>
                             <div style="color:white;font-size:1rem;font-weight:500">
                                 {row.get('company')} — {row.get('location')}
-                            
+                            </div>
                             <div style="color:white;font-size:1rem;font-weight:500">
                                 Match Score: {row.get('match_score', 0)}%
-                            
+                            </div>
                             <div style="color:gray;font-size:0.9rem">
                                 {f"Matched on: {generate_match_reason(resume_text, row['description'])}"
                                  if resume_text else ""}
+                            </div>
                         </div>
                         """,
                         unsafe_allow_html=True
